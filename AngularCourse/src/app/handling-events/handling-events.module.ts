297581962ed39d9e05233b1c19cecbbf03131fc4
Guilherme Silveira <guilherme.silveira@hotmail.com.br>
2020-09-09 22:00:26 -0300

    handling-events module added.

---
+++ AngularCourse/src/app/handling-events/handling-events.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BindingsComponent } from './bindings/bindings.component';
+
+
+
+@NgModule({
+  declarations: [BindingsComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BindingsComponent
+  ]
+})
+export class HandlingEventsModule { }
