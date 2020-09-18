70ddabdccc0aee6b5d93c7399d6c098ae9d9861c
Guilherme Silveira <guilherme.silveira@hotmail.com.br>
2020-09-18 19:26:20 -0300

    Bindings - Last binding: Template Variables

--- AngularCourse/src/app/handling-events/bindings/bindings.component.ts
+++ AngularCourse/src/app/handling-events/bindings/bindings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'handling-events-bindings',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingsComponent implements OnInit {
 
+  imageUrl = "http://lorempixel.com/400/200";
+  colspan = 2;
+  isPrimary = true;
+  isActive = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSave($event) {
+    $event.stopPropagation();
+    console.log("Button clicked!", $event);
+  }
+
+  onDivClicked($event) {
+    console.log("Div clicked!", $event);
+  }
+
+  onKeyUp($event) {
+    console.log("Enter was clicked!", $event);
+  }
+
+  onKeyUpValue($event) {
+    console.log($event.target.value);
+  }
+
+  onKeyUpVariable(value) {
+    console.log(value);
   }
 
 }
