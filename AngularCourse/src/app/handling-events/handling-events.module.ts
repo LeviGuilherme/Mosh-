1cb4364b68ea37f22d9fae9a29cd0fffba45fd8d
Guilherme Silveira <guilherme.silveira@hotmail.com.br>
2020-09-18 20:40:58 -0300

    Custom pipes

--- AngularCourse/src/app/handling-events/handling-events.module.ts
+++ AngularCourse/src/app/handling-events/handling-events.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BindingsComponent } from './bindings/bindings.component';
 import { FormsModule } from '@angular/forms';
+import { PipeComponent } from './pipe/pipe.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
-  declarations: [BindingsComponent],
+  declarations: [BindingsComponent, PipeComponent, SummaryPipe],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
-    BindingsComponent
+    BindingsComponent,
+    PipeComponent
   ]
 })
 export class HandlingEventsModule { }
---
+++ AngularCourse/src/app/handling-events/summary.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+
+  transform(value: string, limit?: number): string {
+    if (!value)
+      return null;
+
+    if (!limit)
+      return value.substring(0, 50) + '...';
+
+    return value.substring(0, limit) + '...';
+  }
+
+}
